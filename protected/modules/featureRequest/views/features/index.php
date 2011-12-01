29a7508b17fab1510ce5d0ef7ec931a4bea3da48
Benjamin Woester <benjamin.woester@gmail.com>
2011-12-01 19:17:04 +0100

    mod: remove commented code

--- protected/modules/featureRequest/views/features/index.php
+++ protected/modules/featureRequest/views/features/index.php
@@ -3,15 +3,6 @@
 /* @var $this FeaturesController */
 /* @var $dataProvider CActiveDataProvider */
 
-//$models = FeatureRequest::model()->findAll();
-//
-//foreach ($models as $model)
-//{
-//  $this->widget( 'featureRequests.widgets.featureRequestSummary.FeatureRequestSummary', array(
-//    'model' => $model,
-//  ));
-//}
-
 $this->widget( 'zii.widgets.CListView', array(
   'dataProvider' => $dataProvider,
   'itemView' => '_featureRequestSummary',
