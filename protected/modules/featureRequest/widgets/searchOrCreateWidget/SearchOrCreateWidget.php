b6cc5120cc727ca6ff32a00a647bc702630a84d2
Benjamin Woester <benjamin.woester@gmail.com>
2011-12-04 16:18:08 +0100

    fix: SearchOrCreateWidget - make the view-on-click_on_result feature work (ugly, needs refactoring)

--- protected/modules/featureRequest/widgets/searchOrCreateWidget/SearchOrCreateWidget.php
+++ protected/modules/featureRequest/widgets/searchOrCreateWidget/SearchOrCreateWidget.php
@@ -76,17 +76,40 @@ class SearchOrCreateWidget extends CJuiWidget
     if (!isset($this->CJuiAutoComplete['options']['select']))
     {
       $this->CJuiAutoComplete['options']['select'] = 'js:function( event, ui ) {
-        var hoveredText = ui.item.'.$this->resultDisplay.';
-        var decoded = $("<div/>").html( hoveredText ).text();
-        $(this).val( decoded );
+        var viewUrl = "'.CHtml::normalizeUrl($this->viewUrl).'";
+        var parsed = parseUri( viewUrl );
 
-        $(this).siblings( "input[type=submit]" ).val( "Go" );
-        $(this).siblings( "input[type=hidden]" ).attr( "name", "id" );
-        $(this).siblings( "input[type=hidden]" ).val( ui.item.'.$this->resultId.' );
-        $(this).closest( "form" ).attr( "action", "'.CHtml::normalizeUrl($this->viewUrl).'" );
-        
-        // TODO: for some reason, this will cause a search, although the the form action has been modified..
-        // $(this).closest( "form" ).submit();
+        // if the URL contains a query string, remove it and render it as
+        // hidden input elements
+        // @see "http://stackoverflow.com/questions/901115/get-query-string-values-in-javascript#answer-2880929"
+        var urlParams = {};
+        (function (query) {
+            var e,
+                a = /\+/g,  // Regex for replacing addition symbol with a space
+                r = /([^&=]+)=?([^&]*)/g,
+                d = function (s) { return decodeURIComponent(s.replace(a, " ")); },
+                q = query; // window.location.search.substring(1);
+
+            while (e = r.exec(q))
+               urlParams[d(e[1])] = d(e[2]);
+        })(parsed.query);
+
+        var queryInputs = "";
+        $.each( urlParams, function(key, value) {
+          queryInputs += \'<input type="hidden" name="\' + key + \'" value="\' + value + \'" />\';
+        });
+
+        var resultId = ui.item.'.$this->resultId.';
+        var viewFormHtml = \'<form action="\' + viewUrl + \'" method="get">\
+          \' + queryInputs + \'\
+          <input type="hidden" value="\' + resultId + \'" name="id" />\
+          <input name="" type="submit" value="Go" />\
+        </form>\';
+
+        $(viewFormHtml)
+          .appendTo("body")
+          .submit()
+        ;
 
         return false;
       }';
